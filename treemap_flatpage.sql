d4ff1f5d39978019ee8494974ba2c02e8ec20092
Dave Michelson <dmichelson@ashevillenc.gov>
2012-12-14 15:42:40 -0500

    adding flatpage inserts

---
+++ treemap_flatpage.sql
@@ -0,0 +1,160 @@
+SELECT pg_catalog.setval('django_flatpage_id_seq', 6, true);
+INSERT INTO django_flatpage (id, url, title, content, enable_comments, template_name, registration_required) VALUES (5, '/terms/', 'Terms and Conditions', '<h3>Acceptable Use</h2>
+
+<h4>Editing and adding trees </h4>
+All users will be granted access to edit and add trees, however action will be taken against edits deemed detrimental to tree mapper application.  By using this application you are agreeing to adhere to these rules
+
+<ul>
+<li>Do not use profanity in any part of this application.</li>
+<li>Refrain from knowingly adding trees that do not exist.  It is acceptable to add tree information  with incorrect information when it is a best guess.  The goal of the AshevilleTreeMapper is to build a tree inventory based on continually user input.</li>
+<li>Refrain from knowingly editing trees with information that is not correct with the intent of degrading the data.</li>
+</ul>
+
+<h4>Grounds for User Removal</h4>
+<ul>
+<li>Use of profanity in any part of this application.</li>
+<li>Verbal Attacking or threatening users in any part of this application.</li>
+<li>Adding two or more trees with the intent of degrading the tree information or application.</li>
+<li>Editing any tree with the obvious intent of degrading the tree information more than two times.</li>
+</ul>
+
+<h4>Grounds for Tree Removal</h4>
+<ul>
+<li>Use of profanity in any part of this application.</li>
+<li>Verbal attacking or threatening users in any part of this application..</li>
+<li>Adding a Tree with the intent of degrading the tree data or application.</li>
+<li>Editing any tree with the obvious intent of degrading the tree information. (only the edited data will be removed)</li>
+</ul>
+
+<h4>Disclaimers</h4>
+
+The tree information contained on this the Website is provided on an â€œas isâ€ basis with no warranties expressed or otherwise implied relating to the accuracy, fitness for purpose, compatibility or security of any components of the Website. 
+
+We do not guarantee uninterrupted availability of the AshevilleTreeMap Website and cannot provide any representation that using the Website will be error free.  
+
+<h4>Third Parties</h4>
+
+This Website may contain hyperlinks to websites operated by other parties. We do not control such websites and we take no responsibility for, and will not incur any liability in respect of, their content. Our inclusion of hyperlinks to such websites does not imply any endorsement of views, statements or information contained in such websites.', false, '', false);
+INSERT INTO django_flatpage (id, url, title, content, enable_comments, template_name, registration_required) VALUES (1, '/faq/', 'FAQs', '    <div id="flatcontent">
+        <h2>How do I use AshevilleTreeMap?</h2>
+<p>There are several ways that you can explore and use AshevilleTreeMap.</p>
+<ol start="1" type="1">
+<li><b>Find Trees:</b> Use AshevilleTreeMap to search for trees near a specific address or in a general neighborhood using the Filter by Location search box located at the top of the page. Type an address, city, and state or select a location by clicking the List button. Click the "Go" button located in the search box to find trees near that location. You can also search by a tree''s species using the Filter by Species search box. Discover which trees are near your home!</li>
+
+<li><b>Add Trees:</b> Do you know of a tree that isn''t listed in AshevilleTreeMap? Add trees near your home or in your neighborhood and help us create a more complete inventory of trees in our region. To add a tree, we recommend first searching AshevilleTreeMap to verify that the tree isn''t already in the site. If it''s not listed, go ahead and add your tree! To add a tree, you will need to <a href="http://http://72.250.240.58/accounts/register/" target="_blank">create a free account</a> and then log in to the system. Once you''ve logged in, visit the <a href="http://http://72.250.240.58/trees/add/" target="_blank">Add a Tree page</a> to enter information about your tree. Fill out the requested information in the three simple steps and then click "Add this Tree."</li>
+
+<li><b>Edit Trees:</b> Many trees in AshevilleTreeMap may need to be updated with new information. <a href="http://http://72.250.240.58/map/" target="_blank">Search AshevilleTreeMap</a> for trees near your home or in your neighborhood. Each green dot indicates the location of a tree. Click the dot to read more information. Do you have something to add about this tree? Click the "Edit Details" link inside the tree''s information box to add or edit information about the tree including its species, location, plot type, and other details. You will need to create a free account and log in to edit trees.</li> 
+</ol>
+<p>Thank you for your assistance in making AshevilleTreeMap a great resource for information on trees Asheville and Buncombe County region!</p>
+
+<h2>How do I find out the species of my tree?</h2>
+<p>Visit the <a href="http://72.250.240.58//OpenTreeMap/treekeys" target="_blank">AshevilleTreeMap Tree Key</a> to research the species  of your tree. The tree key will walk you through the process of determining a  tree species based on leaf structure. If you cannot definitively determine your  tree species, you can still add your tree to AshevilleTreeMap. If possible,  upload images of your tree and its bark, leaves, flowers, or fruit. Other  AshevilleTreeMap users may be able to determine the tree species based on these  images. </p>
+
+<h2>What if the species of my tree is not listed in the tree  key?</h2>
+<p>We tried to include all the major species found in the Asheville region, but  we could easily have missed a few. If your treeâ€™s species is not an option in  AshevilleTreeMap, email NEEDEMAIL to have it added to the system. </p>
+
+<h2>What if the my neighborhood is not listed in the location list?</h2>
+<p>We tried to include all the neighborhoods within the City of Asheville  but its easy to miss one - neighborhood names and boundaries can be very subjective.  If you feel an existing nieghborhood, within the city of asheville is not correct please contact the City of Asheville Neighborhood sevices NEES CONTACT INFO.  Please do the same for any missing City of Asheville Neighborhoods.  Another option is to add the geography to the AshevlleTreeMapper here http://72.250.240.58/OpenTreeMap/_admin_/treemap/neighborhood/ , yes more crowd-sourcing.  </p>
+
+<a name="measure"></a>
+<h2>How do I measure the diameter of my treeâ€™s trunk?</h2>
+<p>Entering the diameter of a tree is crucial for helping track  that treeâ€™s growth and environmental benefits. Measuring the diameter can be a  bit tricky though. The following video from the people at <a href="http://72.250.240.58/OpenTreeMap/treekeys" target="_blank">UrbanForestMap.org</a>  provides a quick tutorial on an easy way to measure your treeâ€™s diameter.</p>
+<p style="text-align:center;">
+<object width="400" height="265"><param value="true" name="allowfullscreen"><param value="always" name="allowscriptaccess"><param value="http://vimeo.com/moogaloop.swf?clip_id=11119129&amp;server=vimeo.com&amp;show_title=0&amp;show_byline=0&amp;show_portrait=0&amp;color=00ADEF&amp;fullscreen=1" name="movie"><embed width="400" height="265" allowscriptaccess="always" allowfullscreen="true" type="application/x-shockwave-flash" src="http://vimeo.com/moogaloop.swf?clip_id=11119129&amp;server=vimeo.com&amp;show_title=0&amp;show_byline=0&amp;show_portrait=0&amp;color=00ADEF&amp;fullscreen=1"></object>
+</p>
+
+<a name="plot_type"></a>
+<h2>What does plot type mean?</h2>
+<p>Plot type refers to the location where the tree is planted. There are a number of different possible plot types. Please select the one that most accurately reflects your tree''s location.</p>
+<ul><li>Median/Island - Trees located in a portion of land between streets or a divided roadway</li>
+  <li>Natural Area - Trees located in a non-developed location that is not maintained as a lawn or recreational area</li>
+  <li>Other - Trees that do not fit in another category (a tree in a rooftop garden, a tree in an interior atrium, etc)</li>
+  <li>Park - Trees located in maintained parks or recreational areas</li>
+  <li>Planter - Trees located in a raised container rather than planted at ground level</li>
+  <li>Tree Lawn - Trees located in a strip of grass located between a street and the sidewalk</li>
+  <li>Well/Pit - Trees located in a rectangular cut in the sidewalk, specifically designated for trees</li>
+  <li>Yard - Trees located in the yard or open space around a home or building</li></ul>
+
+<a name="plot_size"></a>
+<h2>What does plot size mean?</h2>
+<p>Plot size refers to the dimensions of the plot type  where the tree is planted.</p>
+
+<h2>What is the source of the tree data in AshevilleTreeMap?</h2>
+<p>Our data comes from the tree inventory gathered and maintained by the city of Asheville in the last ten years.</p>
+<h2>How can I support urban forestry in Asheville?</h2>
+<p>There are a number of great ways to get involved in caring  for trees and supporting conservation events throughout Asheville.</p>
+<ol start="1" type="1">
+  <li><strong>Add trees to AshevilleTreeMap!</strong> One       of the best ways to ensure that trees are properly cared for and       maintained is to have an accurate and current inventory of the urban tree       population. Do you know of a tree that isnâ€™t in AshevilleTreeMap? Add that       tree now or update existing tree information!</li>
+  <li><strong>Discover</strong> more about how urban       trees can increase energy efficiency, protect our waterways, and generally       make for a more green and enjoyable living space. Check out our Resources       page for more information. <strong></strong></li>
+  <li><strong>Volunteer</strong> NEEDS TEXT</li>
+  <li><strong>Spread the Word!</strong> Tell others about       AshevilleTreeMap and the importance of urban trees. The more tree advocates       we have, the greener we can make our communities!  </li>
+</ol>
+<h2>Can I add tree information for trees outside of Asheville ?</h2>
+<p>Absolutely! AshevilleTreeMap supports the addition of  information for trees in most of Western North Carolina!. </p>
+<h2>My town or community has a lot of tree data. Is there a way  to upload many trees at once?</h2>
+<p>If you are from a town or other community and have a tree  inventory you would like to add to AshevilleTreeMap, please contact NEEDS EMAIL. </p>
+<h2>How can I be sure that the information in AshevilleTreeMap is  accurate?</h2>
+<p>Much of the data comes from City of Asheville who have  carefully surveyed street trees throughout the city. You can help us ensure  that the data is as current and accurate as possible by updating and correcting  information on the trees in your neighborhood. We have implemented a number of  checks that will hopefully prevent the entering of incorrect data. If you find  incorrect information, please correct it!</p>
+<a name="reputation"></a>
+<h2>Why does it list reputation points next to my user name?</h2>
+<p>Any individual can register for a free AshevilleTreeMap account and add trees to the database or edit existing tree information. Public users gain reputation points by contributing to the AshevilleTreeMap database. After reaching 1,000 reputation points, users may edit additional data fields related to the height and status of the tree. Users with this higher reputation level may also review public edits and provide positive (thumbs-up), neutral, or negative (thumbs-down) responses to the edit. Such actions affect the public user''s reputation (i.e. if a more experienced user likes your edit, you get additional reputation points). Reputation points are gained and lost according to the following system.
+<ul><li>Adding a tree is +25 points</li>
+<li>Editing a tree is +5 points</li>
+<li>Receiving a thumbs-up on your edit is +5 points</li>
+<li>Receiving a thumbs-down on your edit is -5 points</li>
+<li>Receiving a neutral reaction to your edit is +1 point</li>
+<li>Achieve advanced user status at 1,000 points</li>
+<li>Users cannot gain or lose more than 100 points per day</li></ul>
+
+<h2>How do I report an issue with a tree or a hazardous or dead tree?</h2>
+<p>AshevilleTreeMap is a collaborative mapping tool and inventory of trees in the greater Asheville region. It is not a tool for reporting issues with trees or potentially hazardous or dead trees. If you are concerned about a public tree, please contact your local government. </p> 
+<h2>Why does it say there is no eco impact data available for my neighborhood?</h2>
+<p>To calculate the eco impact of a tree, the iTree software we use must know the species and diameter of the tree. For many of the trees, we might know the location of the tree but not the exact species and diameter. For this reason, many neighborhoods in Asheville may show lots of trees but not list any eco impact data. You can help us more accurately calculate eco data by entering the species and diameter for trees in your neighborhood.</p>
+<h2>What is the source of the eco impact data? </h2>
+<p>
+We calculated the economic benefits and environmental  impacts of the trees using the <a href="http://www.itreetools.org/" target="_blank">i-Tree software</a> provided by the <a href="http://www.fs.fed.us/" target="_blank">USDA Forest  Service</a>. This software provides options for calculating benefits by assigning a  dollar value to the impact of trees in a number of ecological areas. These  areas include electricity, natural gas, carbon dioxide, particulate matter,  nitrogen dioxide, sulfur dioxide, volatile organic compounds, and stormwater  interception. The USDA Forest Service provides default dollar values for each  of the categories in iTree. We used these default values for carbon dioxide,  particulate matter, nitrogen dioxide, sulfur dioxide, volatile organic  compounds, and stormwater interception as no values specific to the Asheville region were  available. For the electricity fields, we used a price of NEED COST per Kwh based  on local Asheville electricity rates for residential customers. For natural gas rates, we used a  price of NEED COST per therm. To calculate the benefits to a homeowner, we also  entered a median home sale price for Asheville of NEED COST based on data gathered from Trulia.com, the real estate search  site. Based on these numbers, i-Tree calculated the approximate financial  benefits of a tree based on its species and size.</p>
+    </div>
+
+', false, '', false);
+INSERT INTO django_flatpage (id, url, title, content, enable_comments, template_name, registration_required) VALUES (4, '/treekeys/', 'Tree Keys', 'Below are some excellent links to tree keys.  Tree keys will help identify trees species...
+
+<ul>
+    <li>
+       <a  href="http://dendro.cnre.vt.edu/dendrology/idit.htm" target="_blank">Virgina Tech</a>
+   </li>
+    <li><a  href="http://www.ibiblio.org/pic/GymnospKey/start.html" target="_blank">UNC - Library?</a></li>
+    <li>
+       List of North Carolina <a  href=" http://www.duke.edu/~cwcook/trees/#trees" target="_blank">Trees</a> from Duke University.
+   </li>
+   <li>
+      Use your your mobile device to identify a tree <a  href="http://leafsnap.com/">Leafsnap</a>.
+  </li>
+</ul> 
+
+<p>
+Please  <a href="../contact/">contact</a> us to add other tree identification resources!
+</p>', false, '', false);
+INSERT INTO django_flatpage (id, url, title, content, enable_comments, template_name, registration_required) VALUES (3, '/resources/', 'Tree-sources', '<style type="text/css">
+</style>
+
+<h2>Sample Resource</h2>
+<p>some sample resource text</p>
+<h3>Sample Resource Links</h3>
+<a href="http://72.250.240.58/OpenTreeMap/faq/">Sample Resource Link</a> 
+<p>Sample Description:</p>
+<a href="http://72.250.240.58/OpenTreeMap/faq/">Sample Resource Link</a> 
+<p></p>
+<a href="http://www.treesaregood.org/">http://www.treesaregood.org/</a> 
+<p></p>
+<a href="http://www.ncufc.org/">http://www.ncufc.org/</a> 
+<p></p>
+<a href="http://www.urbanforestrysouth.org/">http://www.urbanforestrysouth.org/</a> 
+<p></p>
+', false, '', false);
+INSERT INTO django_flatpage (id, url, title, content, enable_comments, template_name, registration_required) VALUES (2, '/about/', 'About Asheville the Tree Map', '<h2>About<h2>
+
+<h2>History<h2>
+
+<h2>Why?<h2>
+
+<h2>Who?<h2>', false, '', false);
---
+++ treemap_flatpage_sites.sql
@@ -0,0 +1,6 @@
+SELECT pg_catalog.setval('django_flatpage_sites_id_seq', 104, true);
+INSERT INTO django_flatpage_sites (id, flatpage_id, site_id) VALUES (74, 5, 1);
+INSERT INTO django_flatpage_sites (id, flatpage_id, site_id) VALUES (76, 4, 1);
+INSERT INTO django_flatpage_sites (id, flatpage_id, site_id) VALUES (98, 1, 1);
+INSERT INTO django_flatpage_sites (id, flatpage_id, site_id) VALUES (104, 3, 1);
+INSERT INTO django_flatpage_sites (id, flatpage_id, site_id) VALUES (57, 2, 1);
