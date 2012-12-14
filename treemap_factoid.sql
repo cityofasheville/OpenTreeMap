e6863400867828d3990fb8a6d92dd736e19546ad
Dave Michelson <dmichelson@ashevillenc.gov>
2012-12-14 10:54:37 -0500

    added treemap_factoid.sql

---
+++ treemap_factoid.sql
@@ -0,0 +1,6 @@
+INSERT INTO "public"."treemap_factoid" ("fact", "category", "header") VALUES ('test', 'General', 'test');
+INSERT INTO "public"."treemap_factoid" ("fact", "category", "header") VALUES ('test', 'Interesting Quote', 'test');
+INSERT INTO "public"."treemap_factoid" ("fact", "category", "header") VALUES ('test', 'How You Can Help', 'test');
+INSERT INTO "public"."treemap_factoid" ("fact", "category", "header") VALUES ('test', 'Editing', 'test');
+INSERT INTO "public"."treemap_factoid" ("fact", "category", "header") VALUES ('test', 'Interesting Fact', 'test');
+COMMIT;
