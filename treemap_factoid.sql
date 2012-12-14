1e1543b67095d4f2c5744ae4e98f4ed1c7a3ec95
Dave Michelson <dmichelson@ashevillenc.gov>
2012-12-14 13:27:09 -0500

    typo . instead of ,

--- treemap_factoid.sql
+++ treemap_factoid.sql
@@ -1,6 +1,6 @@
-INSERT INTO "public"."treemap_factoid" ("id"."fact", "category", "header") VALUES ('1','test', 'General', 'test');
-INSERT INTO "public"."treemap_factoid" ("id"."fact", "category", "header") VALUES ('3','test', 'Interesting Quote', 'test');
-INSERT INTO "public"."treemap_factoid" ("id"."fact", "category", "header") VALUES ('6','test', 'How You Can Help', 'test');
-INSERT INTO "public"."treemap_factoid" ("id"."fact", "category", "header") VALUES ('9','test', 'Editing', 'test');
-INSERT INTO "public"."treemap_factoid" ("id"."fact", "category", "header") VALUES ('11','test', 'Interesting Fact', 'test');
+INSERT INTO "public"."treemap_factoid" ("id","fact", "category", "header") VALUES ('1','test', 'General', 'test');
+INSERT INTO "public"."treemap_factoid" ("id","fact", "category", "header") VALUES ('3','test', 'Interesting Quote', 'test');
+INSERT INTO "public"."treemap_factoid" ("id","fact", "category", "header") VALUES ('6','test', 'How You Can Help', 'test');
+INSERT INTO "public"."treemap_factoid" ("id","fact", "category", "header") VALUES ('9','test', 'Editing', 'test');
+INSERT INTO "public"."treemap_factoid" ("id","fact", "category", "header") VALUES ('11','test', 'Interesting Fact', 'test');
 COMMIT;
