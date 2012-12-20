4c7b89774863dcb9ea8054bcbab6d3ac13b7583f
Dave Michelson <dmichelson@ashevillenc.gov>
2012-12-20 11:30:18 -0500

    changed send adderess

--- treemap/views.py
+++ treemap/views.py
@@ -1625,7 +1625,7 @@ def contact(request):
             if cc_myself:
                 recipients.append(sender)
 
-            sender = 'dmichelson@ashevillenc.gov' #settings.DEFAULT_FROM_EMAIL
+            sender = 'no-reply@ashevilletreemap.org' #settings.DEFAULT_FROM_EMAIL
 
             from django.core.mail import send_mail
             send_mail(subject, message, sender, recipients)
